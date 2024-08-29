0fd37b5eb10df368fea2dc880c0921eb912117e7
Woojin Son <woojin.son@web.de>
2024-08-29 08:07:14 +0200

    add alignment rolling

--- src/role-list/role-list.cpp
+++ src/role-list/role-list.cpp
@@ -2,6 +2,8 @@
 
 RoleList::RoleList(std::vector<std::string> input, std::vector<ListEntry *> data)
 {
+  std::srand(std::time(nullptr));
+
   this->data = data;
   for (auto entry : input)
   {
@@ -18,36 +20,66 @@ RoleList::RoleList(std::vector<std::string> input, std::vector<ListEntry *> data
 
 std::string RoleList::generate()
 {
-  std::vector<int> counts;
   counts.resize(this->data.size(), 0);
 
   std::string res;
 
   for (auto input_entry : this->query)
   {
+    line++;
+
+    bool role_is_valid = false;
     for (int i = 0; i < this->data.size(); i++)
     {
       std::vector<std::string> aliases = data[i]->aliases;
       if (std::find(aliases.begin(), aliases.end(), input_entry) != aliases.end())
       {
-        if (counts[i] == data[i]->limit) throw Error("Invalid role: %s", input_entry.c_str());
+        if (counts[i] == data[i]->limit) throw Error("Too many of role: %s (at line %d)", data[i]->name, line);
 
         switch (data[i]->type())
         {
         case ListEntry::Type::ROLE:
           res.append(static_cast<Role *>(data[i])->get_colored_str());
-          res.append("\n");
           break;
         case ListEntry::Type::ALIGNMENT:
+        {
+          Role *role = generate_role_from_alignment(i);
+
+          res.append(role->get_colored_str());
+          break;
+        }
         case ListEntry::Type::FACTION:
           break;
         }
 
+        res.append("\n");
+
         counts[i]++;
+        role_is_valid = true;
         break;
       }
     }
+
+    if (!role_is_valid) throw Error("Invalid role name: %s (at line %d)", input_entry.c_str(), line);
   }
 
   return res;
+}
+
+Role *RoleList::generate_role_from_alignment(int i)
+{
+  Alignment *alignment = static_cast<Alignment *>(data[i]);
+  int len = alignment->roles.size();
+
+  if (counts[i] == data[i]->limit) throw Error("Too many of alignment: %s (at line %d)", alignment->name.c_str(), line);
+
+  while (true)
+  {
+    int index = i - len + std::rand() % len;
+
+    if (counts[index] != data[index]->limit)
+    {
+      return static_cast<Role *>(data[index]);
+    }
+  }
 }
--- src/role-list/role-list.hpp
+++ src/role-list/role-list.hpp
@@ -1,11 +1,14 @@
 #ifndef ROLE_LIST_HPP
 #define ROLE_LIST_HPP
 
+#include <functional>
 #include <algorithm>
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cstdlib>
 #include <cctype>
+#include <ctime>
 #include "error/error.hpp"
 #include "list-entry/list-entry.hpp"
 #include "role/role.hpp"
@@ -16,6 +19,11 @@ class RoleList
 {
   std::vector<std::string> query;
   std::vector<ListEntry *> data;
+  std::vector<int> counts;
+
+  unsigned int line = 0;
+
+  Role *generate_role_from_alignment(int i);
 public:
   RoleList(std::vector<std::string> input, std::vector<ListEntry *> data);
 
